f0b96f753ce84502bd35378369f3403577d4a6f8
adarsh <adarshinaganti006@gmail.com>
2026-04-05 01:22:41 +0530

    added login UI

--- src/main/java/com/hotel/Main.java
+++ src/main/java/com/hotel/Main.java
@@ -1,16 +1,16 @@
 package com.hotel;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hotel Management System");
-        Scene scene = new Scene(label, 400, 300);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/LoginView.fxml"));
+        Scene scene = new Scene(loader.load(), 400, 300);
 
         stage.setTitle("Hotel System");
         stage.setScene(scene);
---
+++ src/main/java/com/hotel/controllers/LoginController.java
@@ -0,0 +1,39 @@
+package com.hotel.controllers;
+
+import com.hotel.dao.UserDAO;
+import com.hotel.models.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class LoginController {
+
+	@FXML
+	private TextField usernameField;
+
+	@FXML
+	private PasswordField passwordField;
+
+	@FXML
+	private Label messageLabel;
+
+	private UserDAO userDAO = new UserDAO();
+
+	@FXML
+	public void handleLogin() {
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+
+		try {
+			User user = userDAO.login(username, password);
+
+			if (user != null) {
+				messageLabel.setText("Login successful!");
+				System.out.println("Logged in as: " + user.getRole());
+			} else {
+				messageLabel.setText("Invalid credentials");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
